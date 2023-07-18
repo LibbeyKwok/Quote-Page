e2b2eb397de4dab8cb317d264af2567dd15c2f21
Libbey Kwok <libbeykwok8@gmail.com>
2023-07-18 10:08:02 -0700

    Initial Commit

---
+++ Quote Page/Quote_PageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quote_PageApp.swift
+//  Quote Page
+//
+//  Created by Libbey Kwok on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Quote_PageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
