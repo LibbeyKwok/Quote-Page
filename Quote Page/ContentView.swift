8df19a9651f792873cd81e807c70ddc349608e0e
LibbeyKwok <libbeykwok8@gmail.com>
2023-07-20 13:34:41 -0700

    yay

--- Quote Page/ContentView.swift
+++ Quote Page/ContentView.swift
@@ -1,91 +1,118 @@
-//
-//  ContentView.swift
-//  Quote Page
-//
-//  Created by Libbey Kwok on 7/18/23.
-//
 
+//
+// ContentView.swift
+// Quote Page
+//
+// Created by Libbey Kwok on 7/18/23.
+//
 import Foundation
 import UIKit
 import SwiftUI
-
  var newQuote1 = quote()
 // var newQuote = newQuote1.randQA()
 var num1 = newQuote1.randNum()
-
 struct ContentView: View {
-    var body: some View {
-        NavigationStack{
-            VStack {
+  var body: some View {
+    
+    NavigationStack{
+        VStack{
+            ZStack{
+                Color(hex: "F6EFE8")
+                    .edgesIgnoringSafeArea(.all)
                 ZStack{
                     RoundedRectangle(cornerRadius: 40)
                         .fill(Color(hex: "CDD7B6"))
+                        .shadow(radius:3)
                         .padding(.horizontal, 15)
-                        .padding(.vertical, 10)
-                    VStack{
-                        Text("RevvIT")
-                            .font(.largeTitle)
-                            .padding()
-                        Spacer()
-                            .frame(height: 40)
-//                        randomNum = 3
-                       
-                        Text(newQuote1.quotes[num1])
-                            .font(.title)
-                            .multilineTextAlignment(.center)
-                            .padding()
-                        Text("- \(newQuote1.authors[num1])")
-                            .font(.title)
-                        
-                            .foregroundColor(Color(hex: "EAECE3"))
-                        Spacer()
-                            .frame(height:60)
-                        //CHANGE THE DESTINATION
-                        NavigationLink(destination: homepage()) {
-                            Text("Homepage")
-                            
-
-                        }
+                    .padding(.vertical, 10)}
+                .padding()
+                VStack{
+                    Text("RevvIT")
+                        .font(.largeTitle)
+                        .fontWeight(.semibold)
+                        .shadow(radius: 1)
+                        .padding()
+                    Spacer()
+                        .frame(height: 40)
+                    //            randomNum = 3
+                    Text(newQuote1.quotes[num1])
+                        .font(.title)
+                        .fontWeight(.regular)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                    Text("- \(newQuote1.authors[num1])")
+                        .font(.title)
+                        .fontWeight(.light)
+                        .foregroundColor(Color(hex: "787878"))
+                    Spacer()
+                        .frame(height:60)
+                    //CHANGE THE DESTINATION
+                    NavigationLink(destination: homepage()) {
+                        Text("start")
+                            .foregroundColor(Color.black)
                     }
+                    .buttonStyle(.borderedProminent)
+                    .tint(Color(hex: "F6EFE8"))
+                    .cornerRadius(25)
+                    .shadow(radius: 4)
+                    .controlSize(.large)
+                }
+                .padding()
+            }
+            .toolbar {
+                ToolbarItemGroup(placement: .status) {
+                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                        Image("Habit Tracker")
+                    }
+                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                        Text("journal")
+                         
+                    }
+                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                        Image("home")
+                    }
+                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                        Text("Check-In")
+                    }
+                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                        Text("Todo-List")
+                    }
+
+
                     
                 }
             }
-            .padding()
         }
+      }
     }
-}
-
+  }
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
-
-
-
 extension Color {
-    init(hex: String) {
-        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
-        var int: UInt64 = 0
-        Scanner(string: hex).scanHexInt64(&int)
-        let a, r, g, b: UInt64
-        switch hex.count {
-        case 3: // RGB (12-bit)
-            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
-        case 6: // RGB (24-bit)
-            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
-        case 8: // ARGB (32-bit)
-            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
-        default:
-            (a, r, g, b) = (1, 1, 1, 0)
-        }
-
-        self.init(
-            .sRGB,
-            red: Double(r) / 255,
-            green: Double(g) / 255,
-            blue:  Double(b) / 255,
-            opacity: Double(a) / 255
-        )
+  init(hex: String) {
+    let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+    var int: UInt64 = 0
+    Scanner(string: hex).scanHexInt64(&int)
+    let a, r, g, b: UInt64
+    switch hex.count {
+    case 3: // RGB (12-bit)
+      (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+    case 6: // RGB (24-bit)
+      (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+    case 8: // ARGB (32-bit)
+      (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+    default:
+      (a, r, g, b) = (1, 1, 1, 0)
     }
+    self.init(
+      .sRGB,
+      red: Double(r) / 255,
+      green: Double(g) / 255,
+      blue: Double(b) / 255,
+      opacity: Double(a) / 255
+    )
+  }
 }
--- quote.swift
+++ quote.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class quote: NSObject {
-    var quotes = ["It always seems impossible until it's done", "It does not matter how slowly you go as long as you do not stop." , "Do one thing that scares you everyday.", "Each day comes bearing its gifts. Untie the ribbon.", "Living is the art of getting used to what we didn't expect.", "Kindness is one thing you can't give away. It alwys comes back", "Life is 10% what happens to you and 90% how you react to it.", "Success is liking yourself, liking what you do, and likeing how you do it.", "Success only comes to those who dare to attempt.", "You are your best thing."]
+    var quotes = ["It always seems impossible until it's done.", "It does not matter how slowly you go as long as you do not stop." , "Do one thing that scares you everyday.", "Each day comes bearing its gifts. Untie the ribbon.", "Living is the art of getting used to what we didn't expect.", "Kindness is one thing you can't give away. It always comes back", "Life is 10% what happens to you and 90% how you react to it.", "Success is liking yourself, liking what you do, and likeing how you do it.", "Success only comes to those who dare to attempt.", "You are your best thing."]
     var authors = ["Nelson Mandela", "Confucius", "Eleanor Roosevelt", "Ann Ruth Schabacker", "Eleanor C. Wood", "George Skolsky", "Charles R.", "Maya Angelou", "Mallika Tripathi", "Toni Morrison"]
     var randomNum = 0
     var quoteA = ""
